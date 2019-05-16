45f3069842ca95d1b26d16ff9bb228f34b89a57f
andre <andre.b.becker@student.fh-kiel.de>
2019-05-16 19:13:10 +0200

    moinmoin.r enthält jetzt den Beispielcode von Steffen, der über codeshare.io geteilt wurde

--- moinmoin.R
+++ moinmoin.R
@@ -1,2 +1,27 @@
-#moinmoin
-first <- 8+3 
+# Load
+library(ggplot2)
+library(readr)
+library(dplyr)
+library(lubridate)
+sales <- read.csv("./OpenCampusSales.csv",header = TRUE, sep = ";")
+weather <- read.csv2("./OpenCampusWetter.csv",header = TRUE, sep = ";")
+
+data = merge(sales, weather)
+
+data$Sales = as.double((data$Sales))
+data$Date = as.Date(data$Date)
+data$weekday = as.factor(wday(data$Date))
+data$month = as.factor(month(data$Date))
+data$year = as.factor(year(data$Date))
+data$Date = as.Date(data$Date)
+data$Branch = as.factor(data$Branch)
+# data$wwav = as.factor(data$wwav)
+# model estimation
+
+# beste MOdel bei year + weekday
+mod <- lm(Sales ~ year + weekday, data)
+summary(mod)
+
+
+ggplot(data) +
+  geom_point(aes(x  = Date, y = Sales),stat = 'Identity', fill = 'orange')
